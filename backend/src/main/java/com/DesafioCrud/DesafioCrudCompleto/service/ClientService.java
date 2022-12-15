4a99af2aba7278e24afea267488000e378ec9253
unknown <williamsartijose@hotmail.com>
2022-12-15 10:52:14 +0000

    Tratamento de exceções - Find Category by Id, exception handling

--- backend/src/main/java/com/DesafioCrud/DesafioCrudCompleto/service/ClientService.java
+++ backend/src/main/java/com/DesafioCrud/DesafioCrudCompleto/service/ClientService.java
@@ -2,6 +2,8 @@ package com.DesafioCrud.DesafioCrudCompleto.service;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -11,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.DesafioCrud.DesafioCrudCompleto.dto.ClientDTO;
 import com.DesafioCrud.DesafioCrudCompleto.entities.Client;
 import com.DesafioCrud.DesafioCrudCompleto.repositories.ClientRepository;
+import com.DesafioCrud.DesafioCrudCompleto.service.exceptions.ResourceNotFoundException;
 
 
 
@@ -29,7 +32,7 @@ public class ClientService {
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
-		Client entity =obj.get();
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new ClientDTO(entity);
 	}
 	
